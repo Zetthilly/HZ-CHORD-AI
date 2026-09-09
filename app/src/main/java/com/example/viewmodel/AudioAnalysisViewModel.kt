a864c8aee95ea90aacd775b5fa447c8b473d6dc0
Zetthilly <craigmlmb@gmail.com>
2026-09-09 15:05:32 +0200

    Update AudioAnalysisViewModel.kt

--- app/src/main/java/com/example/viewmodel/AudioAnalysisViewModel.kt
+++ app/src/main/java/com/example/viewmodel/AudioAnalysisViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.viewmodel
 
+import com.example.data.DetectedChord
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
